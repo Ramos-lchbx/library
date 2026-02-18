7f924619ae260c6fd7b38ed715f29ad2843ce0ab
Ramos-lchbx <alejandroramos181298@hotmail.com>
2026-02-18 13:14:38 -0600

    Add ability to add new books

--- script.js
+++ script.js
@@ -18,11 +18,11 @@ addBookToLibrary("Mistborn", "Brandon Sanderson", "639 pages", "already read");
 addBookToLibrary("Mistborn 2", "Brandon Sandersonss", "6309 pages", "not read");
 
 function loopTable(){
-    for (let i = 0; i < myLibrary.length; i++){
-        const body = document.querySelector("tbody")
-        const table = document.getElementById("bookTable");
+    const body = document.querySelector("tbody")
+    body.innerHTML = "";
 
-        body.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++){
+        const table = document.getElementById("bookTable");
         console.log(myLibrary[i]);
         row = table.insertRow(-1)
         cell1 = row.insertCell(0);
@@ -30,12 +30,14 @@ function loopTable(){
         cell3 = row.insertCell(2);
         cell4 = row.insertCell(3);
         cell5 = row.insertCell(4);
+        cell6 = row.insertCell(5);
 
         cell1.textContent = `${myLibrary[i].name}`;
         cell2.textContent = `${myLibrary[i].author}`;
         cell3.textContent = `${myLibrary[i].pages}`;
         cell4.textContent = `${myLibrary[i].read}`;
         cell5.textContent = `${myLibrary[i].id}`;
+        cell6
     }
 }
 
