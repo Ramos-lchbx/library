8faa4b292676b647968c958214e92a5ca8b31da7
Ramos-lchbx <alejandroramos181298@hotmail.com>
2026-02-18 16:41:37 -0600

    Add edit status button and fix bugs

--- script.js
+++ script.js
@@ -14,46 +14,55 @@ function addBookToLibrary(name, author, pages, read) {
     myLibrary.push(newBook);
 }
 
-addBookToLibrary("Mistborn", "Brandon Sanderson", "639 pages", "already read");
-addBookToLibrary("Mistborn 2", "Brandon Sandersonss", "6309 pages", "not read");
-
 function loopTable(){
     const body = document.querySelector("tbody")
     body.innerHTML = "";
 
     for (let i = 0; i < myLibrary.length; i++){
-        const table = document.getElementById("bookTable");
         console.log(myLibrary[i]);
-        row = table.insertRow(-1);
+        row = body.insertRow(-1);
         cell1 = row.insertCell(0);
         cell2 = row.insertCell(1);
         cell3 = row.insertCell(2);
         cell4 = row.insertCell(3);
         cell5 = row.insertCell(4);
         cell6 = row.insertCell(5);
+        cell7 = row.insertCell(6);
 
-        let button = document.createElement("button")
-        button.innerText = "Remove"
-        button.className = "btn_rmv"
+        let delButton = document.createElement("button")
+        delButton.innerText = "Remove"
+        delButton.className = "btn_rmv"
+        delButton.dataset.id = myLibrary[i].id;
+
+        let editButton = document.createElement("button")
+        editButton.innerText = "Edit status"
+        editButton.dataset.id = myLibrary[i].id;
+        editButton.dataset.openEdit = "";
 
         cell1.textContent = `${myLibrary[i].name}`;
         cell2.textContent = `${myLibrary[i].author}`;
         cell3.textContent = `${myLibrary[i].pages}`;
         cell4.textContent = `${myLibrary[i].read}`;
         cell5.textContent = `${myLibrary[i].id}`;
-        cell6.appendChild(button);
+        cell6.appendChild(delButton);
+        cell7.appendChild(editButton);
 
-        button.addEventListener("click", () => {removeBook(button, myLibrary[i])})
+        delButton.addEventListener("click", (e) => {removeBook(e.target.dataset.id)})
+        editButton.addEventListener("click", (e) => {editStatus(e.target.dataset.id)})
     }
 }
 
 const addBookBtn = document.querySelector("[data-open-modal]")
 const modal = document.querySelector("[data-modal]")
-const submit = document.querySelector('button[type="submit"]')
+const submit = document.querySelector("button[data-submit-book]")
 const bName = document.getElementById("bname")
 const bAuthor = document.getElementById("author")
 const bPages = document.getElementById("pages")
 const bRead = document.getElementById("read")
+const openEditModal = document.querySelector("[data-open-edit]")
+const editModal = document.querySelector("[data-edit-modal]")
+const subEdit = document.querySelector("[data-submit-edit]")
+const editString = document.getElementById("stat")
 
 addBookBtn.addEventListener("click", () => {
     modal.showModal();
@@ -64,9 +73,27 @@ submit.addEventListener("click", () => {
     loopTable();
 })
 
-function removeBook(button, index) {
-    button.parentNode.parentNode.remove();
-    console.log(index);
-    myLibrary.splice(index, 1);
-    console.log(`${JSON.stringify(myLibrary)}`);
+function removeBook(id) {
+    const index = myLibrary.findIndex(book => book.id === id)
+
+    if (index !== -1) {
+        myLibrary.splice(index, 1)
+    }
+
+    loopTable();
+
 }
+
+function editStatus(id) {
+    editModal.showModal();
+    const index = myLibrary.findIndex(book => book.id === id)
+    subEdit.addEventListener("click", () => {
+    if (index !== -1) {
+        myLibrary[index].read = editString.value;
+    }
+    editString.value = "";
+
+    loopTable();
+})
+}
+
